186f23d0ec593938b19d250c1380e3935249ea2c
Pavel Sturov <paul.sturov@gmail.com>
2019-09-04 11:02:31 +0300

    Final changes

--- task11 (autocomplete)/src/js/createLayout.js
+++ task11 (autocomplete)/src/js/createLayout.js
@@ -6,19 +6,20 @@ function createLayout() {
     const resultWindow = createElement('div', 'resultWindow');
     const header = createElement('h1');
     const header2 = createElement('h2');
+    const img = document.createElement('img');
+    const body = document.querySelector('body');
 
     const citiesContainer = [];
     for (let i =0; i < 5; i ++) {
         citiesContainer.push(createElement('div'));
     }
 
+    img.setAttribute('src', '../src/img/logo.png');
     input.placeholder = 'I\'m looking for...';
     header.innerText = 'Welcome to Citysearch';
     header2.innerText = 'Choose a City';
 
-    document.body.appendChild(header);
-    document.body.appendChild(wrapper);
-
+    appendElements([header, wrapper, img], body);
     appendElements([header2, input, resultWindow], wrapper);
     appendElements(citiesContainer, resultWindow);
 }
